1dd4c586a06f50745dca353dce9d073268b3b3ab
Admin <Admin@LAPTOP-BBNVQA6E>
2024-06-11 14:18:26 +0530

    Encaps push

---
+++ sample/src/main/java/encapsulation/EncapsulationEg1.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class EncapsulationEg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+EncapsulationEg obj=new EncapsulationEg();
+obj.setName("Hello");
+obj.getName();
+	}
+
+}
